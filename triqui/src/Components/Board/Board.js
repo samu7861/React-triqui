9c42aca5709b67d3c2764da753130366094dd9d1
samu7861 <sfrancog7861@gmail.com>
2023-06-14 09:03:55 -0500

    Initial components "Board"

---
+++ triqui/src/Components/Board/Board.js
@@ -0,0 +1,20 @@
+const Board = ({squares}) => {
+
+    
+
+    return(
+        <div className="board">
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+        </div>
+    )
+}
+
+export default Board
