876db73836e2af2de5e69787fbd93c43611a6c91
samu7861 <sfrancog7861@gmail.com>
2023-06-14 08:57:10 -0500

    First states

--- triqui/src/App.js
+++ triqui/src/App.js
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [turn, setTurn] = useState("X")
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [score, setScore] = useState({
+    X: 0,
+    O: 0,
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
